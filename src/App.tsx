551dd1ff4a3a48f612227b7f17fa6554d291106a
Michael Desanker <mdesanker@gmail.com>
2022-03-16 04:44:41 -0400

    Add dash component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/views/auth/Login";
 import Register from "./components/views/auth/Register";
+import Dashboard from "./components/views/dashboard/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
---
+++ src/components/views/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center p-4 max-w-[364px] sm:w-auto sm:h-auto sm:border border-gray-300 rounded-lg sm:px-8 sm:py-16 gap-8">
+      <div className="h-48 w-48 rounded-full border shadow-lg" />
+      <h1 className="text-4xl text-center">Welcome, John Doe!</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
