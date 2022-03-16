6db46a68575e00e0a4e0ed784dd7367545be1696
Michael Desanker <mdesanker@gmail.com>
2022-03-16 11:10:19 -0400

    Display user info on dashboard

--- src/components/views/dashboard/Dashboard.tsx
+++ src/components/views/dashboard/Dashboard.tsx
@@ -1,8 +1,28 @@
+import { useAppSelector } from "../../../store/hooks";
+import { RootState } from "../../../store/store";
+import { DateTime } from "luxon";
+
 const Dashboard = () => {
+  const { user } = useAppSelector((state: RootState) => state.auth);
+
+  const formattedDate =
+    user &&
+    DateTime.fromISO(user.date.toString()).toLocaleString(
+      DateTime.DATETIME_MED
+    );
+
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center p-4 max-w-[364px] sm:w-auto sm:h-auto sm:border border-gray-300 rounded-lg sm:px-8 sm:py-16 gap-8">
-      <div className="h-48 w-48 rounded-full border shadow-lg" />
-      <h1 className="text-4xl text-center">Welcome, John Doe!</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center p-4 max-w-[364px] sm:w-auto sm:h-auto sm:border border-gray-300 rounded-lg sm:px-8 sm:py-16">
+      <img
+        src={user?.avatar}
+        alt={user?.username}
+        className="h-48 w-48 rounded-full border shadow-lg"
+      />
+      <h1 className="text-4xl text-center py-4">
+        Welcome, {user && user.username}!
+      </h1>
+      <p>Email: {user?.email}</p>
+      <p>Member since: {formattedDate}</p>
     </div>
   );
 };
