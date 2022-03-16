a63c4caf2fbb52a5ed2883703906a6af9a22bec2
Michael Desanker <mdesanker@gmail.com>
2022-03-16 09:34:05 -0400

    Create auth slice

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+// export const {} = authSlice.actions;
+
+export default authSlice.reducer;
